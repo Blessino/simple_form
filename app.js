08a246f490cf63825558d19537bcda9450447278
Ashish <grunch.me1@gmail.com>
2025-04-20 16:05:54 +0545

    output page is working

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
+const express = require('express');
 
+const bodyParser = require('body-parser');
+
+// const path = require('path');
+
+const app = express();
+const port = 3000;
+
+//Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+// app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res) => {
+  res.render('form');
+});
+
+app.post('/submit', (req, res) => {
+  const { name, email } = req.body;
+  res.render('result', { name, email });
+});
+
+app.listen(port, () => {
+  console.log(`App running on port http://localhost:${port}...`);
+});
